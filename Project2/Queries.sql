f3e9073c161b3996d8fcbb7197ec0fca3367784b
Mostafa Mohiuddin Jalal Protik <75379858+protikmostafa083@users.noreply.github.com>
2021-07-21 19:51:10 +0600

    Update Queries.sql

--- Project2/Queries.sql
+++ Project2/Queries.sql
@@ -6,3 +6,19 @@ SUM(CONVERT(int, new_deaths))   AS Total_Death,
 
 FROM PortfolioProject1..coviddeath
 WHERE continent is not NULL
+
+
+--2
+-- looking at percentage of people being vaccinated vs people being affected
+SELECT d.[location], population,
+    SUM(d.new_cases) as TOTAL_CASES, 
+    SUM(CONVERT(int, v.new_vaccinations)) as TOTAL_VACCINATED,
+    (SUM(CONVERT(int, v.new_vaccinations))/population)*100 as POP_VAC_PERCENTAGE
+
+FROM PortfolioProject1..coviddeath AS d
+INNER JOIN PortfolioProject1..covidvaccination AS v
+    ON d.[location] = v.[location]
+    AND d.[date] = v.[date]
+    WHERE d.continent IS NOT NULL
+GROUP BY d.[location], population
+ORDER BY d.[location]

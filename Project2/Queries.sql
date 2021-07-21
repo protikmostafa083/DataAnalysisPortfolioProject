38ae67f6ffaae13d50aa66170ff78350d6c19c06
Mostafa Mohiuddin Jalal Protik <75379858+protikmostafa083@users.noreply.github.com>
2021-07-21 16:46:56 +0600

    Update Queries.sql

--- Project2/Queries.sql
+++ Project2/Queries.sql
@@ -5,3 +5,4 @@ SUM(CONVERT(int, new_deaths))   AS Total_Death,
 (SUM(CONVERT(int, new_deaths))/SUM(new_cases))*100 AS Death_Percentage
 
 FROM PortfolioProject1..coviddeath
+WHERE continent is not NULL
